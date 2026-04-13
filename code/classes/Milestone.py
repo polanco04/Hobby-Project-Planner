8fd02240edf21bb1d86055cf336608ae8252bb4f
Jose Mario Marquez <148010039+josemarioomrqz@users.noreply.github.com>
2026-04-13 15:16:08 -0500

    Cleaning

--- code/classes/Milestone.py
+++ code/classes/Milestone.py
@@ -1,2 +1,17 @@
 # Milestones class
 
+from __future__ import annotations
+
+from datetime import datetime
+from typing import Any
+
+class Milestone:
+    def __init__(
+            self,
+            milestoneId: int,
+            name: str,
+            createAt: datetime,
+            deadline: datetime,
+
+    ):
+        
